00e6120470c251cf665480b5eda750a313220238
Max Mumford <maxmumford@gmail.com>
2014-02-10 22:13:48 +0100

    [ADD] analytics code

--- about/index.php
+++ about/index.php
@@ -33,6 +33,17 @@ foreach(preg_split("/((\r?\n)|(\r\n?))/", $cms_contents) as $line){
 	<link type="text/css" rel="stylesheet" href="<?= $config['root'] ?>css/about.css" />
 	<script type="text/javascript" src="<?= $config['root'] ?>js/about.js"></script>	
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-47936354-1', 'doyouknowmy.name');
+	  ga('send', 'pageview');
+
+	</script>
+
 </head>
 <body>
 	<div id="about">
--- index.php
+++ index.php
@@ -18,6 +18,17 @@ $cms_contents = file_get_contents('https://googledrive.com/host/0B9fGr6w-dfLWYTA
 	<link type="text/css" rel="stylesheet" href="<?= $config['root'] ?>css/index.css" />
 	<script type="text/javascript" src="<?= $config['root'] ?>js/index.js"></script>	
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-47936354-1', 'doyouknowmy.name');
+	  ga('send', 'pageview');
+
+	</script>
+
 </head>
 <body>
 	<header id="banner">
